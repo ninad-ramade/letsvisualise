955b5fc547b1392b159724d37e52fc0dc4edd670
Ninad.Ramade <Ninad.Ramade@IDCW68.IDC.LOCAL>
2016-10-05 15:47:27 +0530

    Initial Commit for Sync

---
+++ administrator/logs/error.php
@@ -0,0 +1,9 @@
+#
+#<?php die('Forbidden.'); ?>
+#Date: 2016-09-24 10:22:54 UTC
+#Software: Joomla Platform 13.1.0 Stable [ Curiosity ] 24-Apr-2013 00:00 GMT
+
+#Fields: datetime	priority clientip	category	message
+2016-09-24T10:22:54+00:00	INFO ::1	joomlafailure	Username and password do not match or you do not have an account yet.
+2016-09-24T11:55:11+00:00	INFO ::1	joomlafailure	Username and password do not match or you do not have an account yet.
+2016-10-03T16:17:03+00:00	INFO ::1	joomlafailure	Username and password do not match or you do not have an account yet.
